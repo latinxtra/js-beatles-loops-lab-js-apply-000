08d719654c22f5695b66a5107881692480f014a0
fab turcios <fabricio.turcios@gmail.com>
2019-03-19 02:25:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // add solution here
+
+const musicians = ['John Lennon', 'Paul McCartney', 'George Harrison', 'Ringo Starr'];
+const instruments = ['Guitar', 'Bass Guitar', 'Lead Guitar', 'Drums'];
+
+// didn't need to add the above because provided (accepts 2 parameters)
+
+function theBeatlesPlay(musicians, instruments) {
+  let arr=[];
+  for (let i=0; i < musicians.length; i++) {
+    const str = musicians[i] + ' plays ' + instruments[i];
+   arr.push(str);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts) {
+  let arr=[]  
+  var i = 0 
+  while (i<facts.length) {
+    const str = facts[i] + "!!!";
+    arr.push(str)
+  }
+  return arr;
+}
