f87aa3ee75bcc553549aad2412c3251c38ec58b8
fab turcios <fabricio.turcios@gmail.com>
2019-03-19 02:39:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function theBeatlesPlay(musicians, instruments) {
 function johnLennonFacts(facts) {
   let i = 0 ;
   while (i<facts.length) {
-    facts = facts[i] + "!!!";
+    facts[i] = facts[i] + "!!!";
     i++;
   }
   return facts;
